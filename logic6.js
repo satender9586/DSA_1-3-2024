a02b927b24de5677d7308e196a7c0de6a55846f0
satender <sksatenderkumar59@gmail.com>
2024-03-28 10:15:43 +0530

    pattern level 1

---
+++ logic6.js
@@ -0,0 +1,42 @@
+// patterns
+
+// 1234
+// 1234
+// 1234
+// 1234
+
+// for (let i = 1; i <= 4; i++) {
+//   let line = "";
+//   for (let j = 1; j <= 4; j++) {
+//     line += j + " ";
+//   }
+//   console.log(line);
+// }
+
+// *
+// **
+// ***
+// ****
+
+// for (let i = 1; i <= 4; i++) {
+//   let line = "";
+//   for (let j = 1; j <= i; j++) {
+//     line += "*" + " ";
+//   }
+//   console.log(line);
+// }
+
+// 1
+// 12
+// 123
+// 1234
+
+// for (let i = 1; i <= 4; i++) {
+//   let line = "";
+//   for (let j = 1; j <= i; j++) {
+//     line += j + " ";
+//   }
+//   console.log(line);
+// }
+
+
