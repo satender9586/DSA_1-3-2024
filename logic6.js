69176d5dc9d32d72f7be259c23fd424b8864e7b1
satender <sksatenderkumar59@gmail.com>
2024-03-29 09:15:36 +0530

    pattern

--- logic6.js
+++ logic6.js
@@ -39,4 +39,47 @@
 //   console.log(line);
 // }
 
+// 1
+// 23
+// 456
+// 78910
 
+// let valu = 1;
+// for (let i = 1; i <= 4; i++) {
+//   let line = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     line += valu + " ";
+//     valu++;
+//   }
+
+//   console.log(line);
+// }
+
+// 1
+// 21
+// 321
+// 4321
+
+// for (let i = 1; i <= 4; i++) {
+//   let line = "";
+//   for (let j = i; j >= 1; j--) {
+//     line += j + " ";
+//   }
+//   console.log(line);
+// }
+
+// console.log("hello");
+
+// AAA
+// BBB
+// CCC
+
+const arr = ["A", "B", "C"];
+for (let i = 0; i < 3; i++) {
+  let line = "";
+  for (let j = 0; j < 3; j++) {
+    line += arr[i] + " ";
+  }
+  console.log(line);
+}
