2441854f2b6a21ebe723df25141770a59119762c
satender <sksatenderkumar59@gmail.com>
2024-04-01 12:27:49 +0530

    Anogram

---
+++ logic10.js
@@ -0,0 +1,22 @@
+function checkSameString(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const obj = {};
+
+  for (let char of str1) {
+    obj[char] = (obj[char] || 0) + 1;
+  }
+
+  for (let char of str2) {
+    if (!obj[char]) {
+      return false;
+    }
+    obj[char]--;
+  }
+
+  return true;
+}
+
+console.log(checkSameString("satender", "satendr"));
